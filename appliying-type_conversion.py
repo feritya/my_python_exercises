366b13d74fa2b79a570f634728d619a585f9f354
ferit yaşar <61287596+feritya@users.noreply.github.com>
2023-02-16 18:14:51 +0300

    type conversion
    I did type conversion

---
+++ appliying-type_conversion.py
@@ -0,0 +1,16 @@
+"""
+program to find the area of ​​the circle and the circumference of the circle
+
+
+area of ​​the circle :pi*r^2
+
+circumference of the circle: 2*pi*r
+
+
+"""
+radius  =input ("enter the radius")
+
+radius =float(radius)
+
+print("circumference of the circle : ",2*3.14*radius)
+print("area of ​​the circle : ",3.14*radius*radius)
---
+++ type_conversion.py
@@ -0,0 +1,75 @@
+a =5
+b = 6.5
+c ="name"
+d = True
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+#type conversion
+#int to float
+"""
+a =float(a)
+print(type(a))
+print(a)
+"""
+#int to string 
+"""
+a =str(a)
+print(type(a))
+print(a)
+"""
+"""
+#int to bool
+a=bool(a)
+print(type(a))
+print(a)
+"""
+"""
+#float to int 
+b=int(b)
+print(type(b))
+print(b)
+"""
+#float to str
+"""
+b =str(b)
+print(type(b))
+print(b)
+"""
+#float to bool
+"""
+b=bool(b)
+print(type(b))
+print(b)
+"""
+#string to  bool
+"""
+c=bool(c)
+print(type(c))
+print(c)
+"""
+#bool to int 
+"""
+d =int(d)
+print(type(d))
+print(d)
+"""
+
+#bool to str
+"""
+d =str(d)
+print(type(d))
+print(d)
+"""
+
+
+total = a+b
+print(type(total))
+print(total)
+
+totalstr=str(a)+str(b)
+print(type(totalstr))
+print(totalstr)
