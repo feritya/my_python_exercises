70be21517a0fac30b2beba297e98179fa7191c44
ferit yaşar <61287596+feritya@users.noreply.github.com>
2023-05-13 02:27:07 +0300

    none

--- selenium_course/workspace/test_py.py
+++ selenium_course/workspace/test_py.py
@@ -5,20 +5,20 @@ from time import sleep
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support  import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
-
-
-class   Test_Demmo:
+from pathlib import Path
+from datetime import date
+class  Test_Demmo:
     def setup_method(self):
         self.driver = webdriver.Chrome(ChromeDriverManager().install())
         self.driver.maximize_window()
         self.driver.get("https://www.saucedemo.com/")
-        
+        self.today = str(date.today())
+        Path = (self.folderPath).mkdir( exist_ok=True) 
     def teardown_method(self):
         self.driver.quit()    
-    def test_func_demo(self):
-        a ="hello"
-      
-    def test_invalid_login(self):
+    
+    @pytest.mark.parametrize("username,password",[("1","1"),("kullaniciadım","sifre")])
+    def test_invalid_login(self,username,password):
         WebDriverWait(self.driver,10).until(EC.presence_of_element_located((By.ID,"user-name")))
         usernameInput = self.driver.find_element(By.ID,"user-name")
         WepDriverWait(self.driver,10).until(EC.presence_of_element_located((By.ID,"password")))
@@ -26,10 +26,13 @@ class   Test_Demmo:
 
         usernameInput.send_keys("1")
         passwordInput.send_keys("1")
-        
+
         LoginBtn=self.driver.find_element(By.ID,"login-button")
         LoginBtn.click()
         errorMessage = self.driver.find_element(By.XPATH,"//*[@id='login_button_container']/div/form/div[3]/h3")
         assert errorMessage.text == "Epic sadface: Username and password do not match any user in this service"
-
+    def test_func_demo(self):
+            a=1
+            if a==1:
+                assert True
 
